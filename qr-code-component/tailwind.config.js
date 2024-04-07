91a1f6db22c7c6cc1d9f1f24e323cf28dfb41ac8
Sina Kh. <97638468+30vam@users.noreply.github.com>
2024-04-07 23:10:26 +0200

    Front-end Mentor Init

---
+++ qr-code-component/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'link-color': 'hsl(228, 45%, 44%)',
+        'light-gray': 'hsl(212, 45%, 89%)',
+        'grayish-blue': 'hsl(220, 15%, 55%)',
+        'dark-blue': 'hsl(218, 44%, 22%)'
+      },
+    },
+  },
+  plugins: [],
+}
