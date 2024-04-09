f23526a9985eed3e835e239ffca159f4004b3a11
Sina Kh. <97638468+30vam@users.noreply.github.com>
2024-04-09 15:31:03 +0200

    Added recipe page

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'primary-green': 'hsl(75, 94%, 57%)'
+        'primary-green': 'hsl(75, 94%, 57%)',
+        'primary-nutmeg': 'hsl(14, 45%, 36%)',
+        'primary-dark-raspberry': 'hsl(332, 51%, 32%)',
+        'eggshell': 'hsl(30, 54%, 90%)',
+        'light-gray': 'hsl(30, 18%, 87%)',
+        'rose-white': 'hsl(330, 100%, 98%)',
+        'wenge-brown': 'hsl(30, 10%, 34%)',
+        'dark-charcoal': 'hsl(24, 5%, 18%)'
+      },
+      fontFamily: {
+        'outfit': ['Outfit', 'sans-serif'],
+        'young-serif': ['Young Serif', 'serif']
       }
     },
   },
