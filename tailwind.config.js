49aeb1b602e2519c10b1cbead5e2df6d155a5eb2
Sina Kh. <97638468+30vam@users.noreply.github.com>
2024-04-16 01:14:57 +0200

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,11 +11,17 @@ module.exports = {
         'light-gray': 'hsl(30, 18%, 87%)',
         'rose-white': 'hsl(330, 100%, 98%)',
         'wenge-brown': 'hsl(30, 10%, 34%)',
-        'dark-charcoal': 'hsl(24, 5%, 18%)'
+        'dark-charcoal': 'hsl(24, 5%, 18%)',
+        'dark-cyan': 'hsl(158, 36%, 37%)',
+        'cream': 'hsl(30, 38%, 92%)',
+        'very-dark-blue': 'hsl(212, 21%, 14%)',
+        'dark-grayish-blue': 'hsl(228, 12%, 48%)'
       },
       fontFamily: {
         'outfit': ['Outfit', 'sans-serif'],
-        'young-serif': ['Young Serif', 'serif']
+        'young-serif': ['Young Serif', 'serif'],
+        'montserrat': ['Montserrat', 'sans-serif'],
+        'fraunces': ['Fraunces', 'serif']
       }
     },
   },
